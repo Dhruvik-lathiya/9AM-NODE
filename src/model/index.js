42525211d572c61b8a46b4e465508aefba89a7ed
Dhruvik-lathiya <dhruviklathiya0811@gmail.com>
2024-06-22 10:07:56 +0530

    populate

--- src/model/index.js
+++ src/model/index.js
@@ -4,6 +4,9 @@ module.exports.Movie = require("./movie.model.js")
 module.exports.Series = require("./series.model.js")
 module.exports.Admin = require("./admin.model.js")
 module.exports.Doc = require("./documentary.model.js")
+module.exports.City = require("./city.model.js")
+module.exports.State = require("./state.model.js")
+module.exports.Country = require("./country.model.js")
 
 
 
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,6 +4,9 @@ const movieRoute = require("./movie.routes")
 const seriesRoute = require("./series.routes")
 const adminRoute = require("./admin.routes")
 const docRoute = require("./documentary.routes")
+const cityRoute = require("./city.routes")
+const stateRoute = require("./state.routes")
+const countryRoute = require("./country.routes")
 
 const router = express()
 
@@ -12,5 +15,8 @@ router.use("/movie",movieRoute)
 router.use("/series",seriesRoute)
 router.use("/admin",adminRoute)
 router.use("/doc",docRoute)
+router.use("/city",cityRoute)
+router.use("/state",stateRoute)
+router.use("/country",countryRoute)
 
 module.exports = router
